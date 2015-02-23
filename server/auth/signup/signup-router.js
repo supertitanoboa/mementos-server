cbb74ca3255add50fe4a2a88bf92861691b18aa8
Omar Duarte <omarduarte@gmail.com>
2015-02-23 12:22:49 -0800

    Adds user signup and login functionality
    Adds user signup functionality
    
    - Adds db models into server requests
    We included the db models object into every request received by
    the server. This way, we can access the bookshelf models from
    every request handler we desire.
    
    - Fixes a bug in the User.setPassword method
    There was a `this` closure created before an inner function was
    called in this methods. One of the lines inside this inner function
    used `this` as opposed to `_this` <- closure
    
    - Adds user signup functionality
    We added the user save functionality in the POST /auth/signup requests
    handler. Sends back a 201 and a sessionID cookie when the user is created,
    or a `409 Conflict` when the user already exists.
    
    Add user login functionality
    
    - Includes the `cors` express library
    Since our mobile app is not being hosted in the same domain as our server,
    we need to handle Cross Origin Resource Sharing headers. The `cors`
    express library handles this really well.
    
    - Fixes problem with user signup
    There was a small bug with user signups. There was no response if the user
    had an invalid password. This is now fixed.
    
    - Adds user login functionality
    Users are now able to login.
    
    - TODO:
    Session handling
    
    Fix: Adds new line at end of login-router

--- server/auth/signup/signup-router.js
+++ server/auth/signup/signup-router.js
@@ -1,20 +1,37 @@
 var express = require('express');
 var passport = require('../../passport.js');
 var signupRouter = express.Router();
+// var db = app.get('db');
 
 signupRouter.get('/', function(req, res) {
   'use strict';
+  //console.log('Request', req);
   // res.redirect('/');
   res.status(200).send('reached the signup page');
 });   
 
 // FIXME: needs proper response after successful authentication
 signupRouter.post('/', 
-  passport.authenticate('local'),
+  // passport.authenticate('local'),
   function(req, res) {
     'use strict';
-    // res.redirect('/');
-    res.status(201).send('things');
+    var user = new req.db.Users({email: req.body.email});
+
+    user.fetch()
+    .then(function(model) {
+      if (!model) {        
+        user.setPass(req.body.password)
+        .then(function(user) {
+          return user.save()
+        })
+        .then(function(model) {                    
+          res.cookie('sessionID', req.sessionID);
+          res.status(201).send('User created!');
+        });
+      } else {
+        res.status(409).send('User Already Exists.');
+      }
+    });
   }); 
 
 module.exports = signupRouter;
